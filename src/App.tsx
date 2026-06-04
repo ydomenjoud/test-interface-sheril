abf3384ce2fa737ded01f3c3158362f95135def8
yannick <yannick@accolade.coop>
2026-06-04 12:03:26 +0200

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
-import Header from './components/Header';
 import Carte from './screens/Carte';
 import ListeSystemes from './screens/ListeSystemes';
 import ListeFlottes from './screens/ListeFlottes';
@@ -12,32 +11,30 @@ import ListePlans from './screens/ListePlans';
 import CreatePlan from './screens/CreatePlan';
 import RechercheTechnologique from './screens/RechercheTechnologique';
 import PlayerSystemDetail from './screens/PlayerSystemDetail';
+import MainLayout from "./components/MainLayout";
 
 function App() {
-    return (<ReportProvider>
-            <div className="app-root">
-                <Header/>
-                <div className="app-body" style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
+    return (
+        <ReportProvider>
+            <Routes>
+                {/* 1. TOUTES LES PAGES AVEC LE LAYOUT PRINCIPAL */}
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Carte />} />
+                    <Route path="/systemes" element={<ListeSystemes />} />
+                    <Route path="/flottes" element={<ListeFlottes />} />
+                    <Route path="/technologies" element={<ListeTechnologies />} />
+                    <Route path="/arbre-technologies" element={<ArbreTechnologies />} />
+                    <Route path="/plans" element={<ListePlans />} />
+                    <Route path="/plans/creer" element={<CreatePlan />} />
+                    <Route path="/recherche" element={<RechercheTechnologique />} />
+                    <Route path="/player-system-detail/:key" element={<PlayerSystemDetail />} />
+                </Route>
 
-                    <main
-                        className="app-main"
-                        style={{flex: 1, minHeight: 0, display: 'flex', flexDirection: 'column'}}
-                    >
-                        <Routes>
-                            <Route path="/" element={<Carte/>}/>
-                            <Route path="/systemes" element={<ListeSystemes/>}/>
-                            <Route path="/flottes" element={<ListeFlottes/>}/>
-                            <Route path="/technologies" element={<ListeTechnologies/>}/>
-                            <Route path="/arbre-technologies" element={<ArbreTechnologies/>}/>
-                            <Route path="/plans" element={<ListePlans/>}/>
-                            <Route path="/plans/creer" element={<CreatePlan/>}/>
-                            <Route path="/recherche" element={<RechercheTechnologique/>}/>
-                            <Route path="/player-system-detail/:key" element={<PlayerSystemDetail/>}/>
-                        </Routes>
-                    </main>
-                </div>
-            </div>
-        </ReportProvider>);
+                {/* 2. PAGE HORS LAYOUT (Pas de Header, pas de styles imposés par le main) */}
+                <Route path="/creer-plan-simple" element={<CreatePlan />} />
+            </Routes>
+        </ReportProvider>
+    );
 }
 
 export default App;
